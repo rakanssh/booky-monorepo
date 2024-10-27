1ce048285c8e7b4dfe022f8bf7a33339124985c7
rakanssh <rsshammri@gmail.com>
2024-10-27 18:49:22 +0300

    first commit

---
+++ backend/src/main/java/co/rakan/booky/BookyApplication.java
@@ -0,0 +1,13 @@
+package co.rakan.booky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookyApplication.class, args);
+	}
+
+}
