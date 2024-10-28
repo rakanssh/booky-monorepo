0d15755876d0f776f3192a32842603152a2fa9ff
rakanssh <rsshammri@gmail.com>
2024-10-28 19:52:34 +0300

    feat: findByOlid

--- backend/src/main/java/co/rakan/booky/repository/AuthorRepository.java
+++ backend/src/main/java/co/rakan/booky/repository/AuthorRepository.java
@@ -1,9 +1,9 @@
 package co.rakan.booky.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import java.util.Optional;
 import co.rakan.booky.model.Author;
 
 public interface AuthorRepository extends JpaRepository<Author, Long> {
-
+    Optional<Author> findByOlid(String olid);
 }
