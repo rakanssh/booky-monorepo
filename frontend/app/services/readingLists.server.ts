003a8395d46fb62c049fae1e78ef979790d4694a
rakanssh <rsshammri@gmail.com>
2024-10-30 00:15:00 +0300

    fix: type inference

--- frontend/app/services/readingLists.server.ts
+++ frontend/app/services/readingLists.server.ts
@@ -1,6 +1,7 @@
+import { ReadingList } from "../types";
 import { api } from "./api.server";
 
 export const readingListsService = {
   createReadingList: async (name: string) =>
-    api.post("/api/reading-lists", { name }),
+    api.post<ReadingList>("/api/reading-lists", { name }),
 };
