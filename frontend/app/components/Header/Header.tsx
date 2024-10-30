cde1fd64dda8da0d06b40ca48cbcd4866387c216
rakanssh <rsshammri@gmail.com>
2024-10-30 18:52:39 +0300

    feat: header simplification

--- frontend/app/components/Header/Header.tsx
+++ frontend/app/components/Header/Header.tsx
@@ -1,11 +1,4 @@
-import { IconSun, IconMoon } from "@tabler/icons-react";
-import {
-  Title,
-  Group,
-  ActionIcon,
-  useMantineColorScheme,
-  Burger,
-} from "@mantine/core";
+import { Title, Group, Burger } from "@mantine/core";
 
 export interface HeaderProps {
   navbarOpen: boolean;
@@ -13,9 +6,6 @@ export interface HeaderProps {
 }
 
 export default function Header({ navbarOpen, setNavbarOpen }: HeaderProps) {
-  const { colorScheme, toggleColorScheme } = useMantineColorScheme();
-  const isDark = colorScheme === "dark";
-
   return (
     <Group justify="space-between" align="center" pt="xs" px="lg" w="100%">
       <Burger
@@ -26,14 +16,6 @@ export default function Header({ navbarOpen, setNavbarOpen }: HeaderProps) {
       <Title order={1} fw={700} fz="xl" style={{ lineHeight: 1, margin: 0 }}>
         Booky
       </Title>
-      <ActionIcon
-        variant="outline"
-        color={isDark ? "yellow" : "blue"}
-        onClick={() => toggleColorScheme()}
-        title="Toggle color scheme"
-      >
-        {isDark ? <IconSun size="1.1rem" /> : <IconMoon size="1.1rem" />}
-      </ActionIcon>
     </Group>
   );
 }
